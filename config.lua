a5117c2e6097403d1fa50d76475098de0eb369eb
justin <jjzwink@gmail.com>
2019-08-10 06:18:14 -0400

    Setup
    Setup of Github and small code push

---
+++ config.lua
@@ -0,0 +1,17 @@
+Config = {}
+
+Config.Locale = 'en'
+
+Config.AdvancedMedical = true -- enable AdvancedMecical? More Indepth Medical treatment Required for use
+
+Config.ESXEnabled = true
+Config.ESXJobWhitlisted = false
+
+
+
+Config.LicenseEnable = true -- enable medical licenses? The player will be required to buy a license in order to pratice medical. Requires esx_license
+
+Config.LicensePrices = {
+    medical_EMS = {label = _U('licenes_ems'), price = 20000}
+    medical_DOCTOR = {label = _U('licenes_doctor'), price = 200000}
+}
---
+++ functions.lua
@@ -0,0 +1,6 @@
+function alert(msg)
+    SetTextFormat("STRING")
+    AddTextComponentString(msg)
+    DisplayHelpFromStringLabel(0,0,1,-1)
+end
+
